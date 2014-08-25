44e678f67d8379cb1e87e2fc265a8541333f5fe7
MonKe <kevin.thommy@gmail.com>
2014-08-25 13:02:50 +0200

    added flags handling

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 var main = {
+  // dom bank
   $: {},
+  // parsed json data
   data: null,
+  // loading state, gets turned off after fist post display
   loading: true,
+  // initial setup
   start: function () {
+    // fill up the dom bank
     main.$ = {
       loader: document.querySelector (".loader"),
       post: document.querySelector (".post"),
@@ -12,13 +17,19 @@ var main = {
       newer: document.querySelector (".newer"),
       older: document.querySelector (".older")
     }
+    // async retrieval of the post data
     xhr.send ("posts.json", function (err, response) {
       if (err) return console.log ("error loading data")
-      main.data = JSON.parse (response).posts
+      // data is published posts only
+      main.data = JSON.parse (response).posts.filter (function (x) {
+        return x.flags.indexOf ("draft") === -1
+      })
       if (main.data.length < 1) return console.log ("no data")
+      // stripping hash char from id before load
       main.load (window.location.hash.replace ("#", ""))
     })
   },
+  // load function, called everytime the hash is updated
   load: function (id) {
     var idlist = main.data.map (function (x) {
       return x.id
@@ -26,7 +37,11 @@ var main = {
         index = idlist.indexOf (id) >= 0 ? idlist.indexOf (id) : 0,
         post = main.data[index]
     // update html
-    main.$.img.setAttribute ("src", post.img)
+    if (post.flags.indexOf ("text-only") !== -1) main.$.img.setAttribute ("class", "post_img hide")
+    else {
+      main.$.img.setAttribute ("class", "post_img")
+      main.$.img.setAttribute ("src", post.img)
+    }
     main.$.news.innerHTML = "&nbsp;<br/>" + post.news.join ("<br/>")
     main.$.stamp.innerHTML = post.stamp
     // reveal the post if hidden
